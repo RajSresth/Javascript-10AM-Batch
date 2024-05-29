9836773754ac7e74f7bec11aa1f7fd989b0738ea
Shresth Rajput <rajshresth2@gmail.com>
2024-05-29 10:59:41 +0530

    Lectur on Type Coercion

--- Day-5 Type Coercion/script.js
+++ Day-5 Type Coercion/script.js
@@ -9,6 +9,102 @@
 // const userName=+prompt('Enter your Name')
 // console.log(userName,typeof(userName))
 
-const a="10"
-const b= +a
-console.log(b,typeof(b))
+// const a="10"
+// const b= +a
+// console.log(b,typeof(b))
+
+
+// convert a string into a number
+// const a=`10`;
+
+// const num=Number(a)
+
+// ES6  +
+
+// const num=+a
+// console.log(num)
+// console.log(typeof num)
+
+
+// userinput
+// const age=Number(prompt('Enter your age'));
+// const age=+prompt('Enter your age');
+// console.log(age,typeof age)
+
+
+// convert strict string into number
+// const name="Chombu"
+
+// const num=+name
+// console.log(num,typeof num)
+
+
+// convert boolean into number
+// const a=10>50
+// const res=+a
+// console.log(res,typeof res)
+
+// convert number into Bigint
+// const a=10;
+// const big_num=BigInt(a)
+// console.log(big_num)
+// console.log(typeof big_num)
+
+// convert string into bigint
+// const a="abc"
+// const a="10"
+// const b=BigInt(a)
+// console.log(b)
+// console.log(typeof b)
+
+
+// convert boolean into bigint
+// const a=true
+// const b=BigInt(a)
+// console.log(b,typeof b)
+
+
+// convert number into string
+// const a=10;
+// const str=String(a)
+// console.log(str)
+// console.log(typeof str)
+
+
+// convert number into boolean
+// const a=0.01;          //0--> only zero is converted into false
+// const res=Boolean(a)
+// console.log(res)
+// console.log(typeof res)
+
+
+// convert string into boolean
+// const str=''
+// const res=Boolean(str)
+// console.log(res)
+// console.log(typeof res)
+
+// string 'abc' ----> true
+// empty string '' ----> false
+
+
+// const userName=prompt('Enter your full name')
+
+// if(userName){
+//     alert(`User ${userName} Login Successfully`)
+// }
+// else{
+//     alert('Enter a valid name...!')
+// }
+
+
+// convert null into number
+// const sal=null;
+// const num=+sal
+// console.log(num,typeof num)
+
+
+// const name=new String("bharat")
+// console.log(name.charCodeAt(0))
+
+
