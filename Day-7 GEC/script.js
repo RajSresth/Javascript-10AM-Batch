14bb22e6be248242c9d37e8177dd65ccb8cf2b5a
Shresth Rajput <rajshresth2@gmail.com>
2024-06-03 11:47:03 +0530

    Lecture on Hoisting and TDZ

--- Day-7 GEC/script.js
+++ Day-7 GEC/script.js
@@ -1,29 +1,41 @@
 // question1
-console.log("start")
-var a=10;
-let b=20;
-const c=30;
-        {
-            var a=100;
-            let b=200;
-            const c=300;
-            console.log(a)
-            console.log(b)
-            console.log(c)
-        }
-console.log(a)
-console.log(b)
-console.log(c)
-console.log("End")
+// console.log("start")
+// var a=10;
+// let b=20;
+// const c=30;
+//         {
+//             var a=100;
+//             let b=200;
+//             const c=300;
+//             console.log(a) 
+//             console.log(b)
+//             console.log(c)
+//         }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log("End")
 
+// output:- start,100,200,300,100,20,30,end
 
 // question2
-console.log("start")
-    let a=10
-    {
-        a=100;
-        let a=100;
-        console.log(a)
-    }
-console.log(a)
-console.log("end")
+// console.log("start")
+//     let a=10
+//     {
+//         a=100;
+//         let a=100;
+//         console.log(a)
+//     }
+// console.log(a)
+// console.log("end")
+
+// question3
+
+// console.log("start")
+// var a;
+// let b;
+// const c=30;
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log("end")
---
+++ Day-8 Hoisting and TDZ/script.js
@@ -0,0 +1,22 @@
+// question2
+// console.log("start");
+// let a = 10;
+// console.log(b);
+// {
+//   var b = 200;
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+
+// question3
+console.log("start");
+let a = 10;
+{
+  console.log(a);
+  let a = 10;
+}
+console.log(a);
+console.log(b);
+console.log("end");
