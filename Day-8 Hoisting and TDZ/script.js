707fc18ec8cfd7d89f9022f193c9c8631bb102b4
Shresth Rajput <rajshresth2@gmail.com>
2024-06-04 11:44:39 +0530

    Lecture on Introduction to Functions

--- Day-8 Hoisting and TDZ/script.js
+++ Day-8 Hoisting and TDZ/script.js
@@ -11,12 +11,107 @@
 
 
 // question3
-console.log("start");
-let a = 10;
-{
-  console.log(a);
-  let a = 10;
-}
-console.log(a);
-console.log(b);
-console.log("end");
+// console.log("start");
+// let a = 10;
+// {
+//   console.log(a);
+//   let a = 10;
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+// Question4
+// console.log("start");
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 100;
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+// Question5
+// console.log("start");
+// let a = 10;
+// var b = 20;
+// const c = 30;
+// {
+//   let a = 10;
+//   console.log(a);
+//   const c = 300;
+//   console.log(b);
+//   b = 200;
+//   c = 30;
+//   console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+// console.log("end");
+
+
+// Question6
+// console.log("start")
+// console.log(a)
+// var a=10;
+// let b=20;
+// const c=30;
+//   {
+//       console.log(b)
+//       let b=100;
+//       a=100;
+//       b=200;
+//       c=300
+//       console.log(a)
+//       console.log(b)
+//       console.log(c)
+//   }
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log("end")
+
+ // Question7
+//  console.log("start")
+// console.log(a)
+// var a=10;
+// let b=20;
+// const c=30;
+//   {
+     
+//       let b=100;
+//       a=100;
+//       b=200;
+//       c=300
+//       console.log(a)
+//       console.log(b)
+//       console.log(c)
+//   }
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log("end")
+
+// Question8
+// console.log("start")
+// console.log(a)
+// var a=10;
+// let b=20;
+// const c=30;
+//   {
+     
+//       let b=100;
+//       a=100;
+//       b=200;
+//       console.log(a)
+//       console.log(b)
+//       console.log(c)
+//   }
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log("end")
---
+++ Day-9 Function in JS/script.js
@@ -0,0 +1,8 @@
+function meme1(person){
+    console.log(`${person} Bhaia mere muh se nikal meri juban fisal gayi....`)
+}
+
+function meme2(person="Basanti"){
+    console.log(`${person} Me bhi tootla mela baap bhi totla mela pula khandan totla`)
+}
+meme2()
