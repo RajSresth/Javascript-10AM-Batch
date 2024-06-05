21d2f391153deecead6a5e5ac6544838dbce8cf2
Shresth Rajput <rajshresth2@gmail.com>
2024-06-05 11:47:50 +0530

    Lecture on Hoisting in Function Declaration Statement

---
+++ Day-10 Function Declaration Statement/script.js
@@ -0,0 +1,26 @@
+
+// const input=prompt('Enter your Full Name')
+
+// function greet(user){
+//     let name=user===""|| user===" "?"Ritesh":user
+//     console.log(`Namaste ${name}`)
+// }
+// greet(input)
+
+
+// function loot(){
+//  console.log("Start")   
+//  let totalAmount=1000*5
+//     return totalAmount
+        
+// }
+
+// const paiseKamaye=loot()
+// console.log(paiseKamaye)
+
+// function sayHii(name){
+//     console.log(`Hi ${name}`)
+// }
+
+// sayHii('Ram')
+// console.log(sayHii)
---
+++ Day-10 Hoisting in Function Declaration/script.js
@@ -0,0 +1,23 @@
+// greet()
+
+// function greet() {
+//     console.log("Hello World")
+// }
+
+console.log("Start")
+let a=1;
+var b=2;
+const c=3;
+greet()
+    function greet()
+    {
+        var b=20;
+        console.log(a)
+         a=10
+        console.log(b)
+        console.log(c)
+    }
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log("End")
