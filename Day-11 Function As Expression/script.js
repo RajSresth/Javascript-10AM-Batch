fbbdb4f8d69a5f23f92ef8d2af353681ab796610
Shresth Rajput <rajshresth2@gmail.com>
2024-06-06 11:25:07 +0530

    Lecture on Function Expression and IIFE

---
+++ Day-11 Function As Expression/script.js
@@ -0,0 +1,36 @@
+// First Class Function
+
+// const a=+prompt('ENter First number');
+// const b=+prompt('ENter Second number');
+
+// const findSum=function(a,b)
+//                 {
+//                     const sum=a+b;
+//                     return sum
+//                 }
+// console.log(`Sum of ${a} and ${b} is: `,findSum(a,b))
+
+   
+
+    // const dialog1=function (name="Basanti") {
+    //     console.log(`${name} in kutto ke samne mat nachna.....`)
+    // }
+
+
+    console.log(meme1)  
+    console.log(typeof meme1)    
+    var meme1=function(name){
+        console.log(`${name} Bhai dekh vo tujhe hi dekh rahi h`)
+    }
+    meme1("Sarthak")
+    console.log(typeof meme1)    
+    
+
+  
+    
+
+
+            
+
+
+    
---
+++ Day-11 IIFE/script.js
@@ -0,0 +1,10 @@
+// Immediate Invoke Function Expression (IIFE)
+// Anonymous Function = Lawarish Function
+
+let response;
+
+(function (){
+    response=confirm("Do you want to share your location..?")
+    
+})()
+console.log('User Response is: ',response)
