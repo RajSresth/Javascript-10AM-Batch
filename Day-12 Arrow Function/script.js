003b4278df0bce74fcfe8156bac75758f734d644
Shresth Rajput <rajshresth2@gmail.com>
2024-06-07 11:35:42 +0530

    Lecture on Arrow Function

---
+++ Day-12 Arrow Function/script.js
@@ -0,0 +1,64 @@
+// const findSum=(a,b)=>{
+//     const sum=a+b;
+//     return sum;
+// }
+
+// const sum=(a,b)=>a+b;
+// const sub=(a,b)=>a-b;
+// const prod=(a,b)=>a*b;
+// const div=(a,b)=>a/b;
+// const pow=(a,b)=>a**b;
+
+// const result=pow(10,4)
+// console.log(result)
+
+
+// Wajsp to find palindrome number?
+
+    const findPalindrome=(num)=>{
+        let a=num;
+        let rev=0;
+        while(a>0)
+        {
+            const rem=a%10 ;
+            rev=rev*10+rem;
+            a=Math.floor(a/10);
+        }
+       if(rev===num)
+        {
+            console.log(`${num}: is Palindrome Number`)
+        }
+        else{
+            console.log(`${num}: is not a Palindrome Number`)
+        }
+    }
+
+    let x=+prompt('Enter any Number')
+    findPalindrome(x)
+
+
+//Write an arrow function and pass a number to find palindrom number between 1 to 500?
+
+//Write an arrow function that takes two number and calculate power?
+
+//write an arrow function that find sum of 100 natural numbers?
+
+//write an arrow function that find sum of square of 100 natural numbers?
+
+//Write an arrow function that count a number?
+
+// Write an arrow function that reverse a number 12345 into 54321?
+
+//Write an arrow function to find strong number?
+
+//write an arrow function to find Armstrong Number?
+
+//Write an arrow function to find Perfect Number?
+
+//Write an arrow function to find factorial of (7!-4!)/3! ?
+
+// Write an arrow function to solve this question?
+// (5^3 -3^3)/2^3
+
+// Wajsp to find palindrom number withou coverting it into string, array and without taking temporary variable?
+    
