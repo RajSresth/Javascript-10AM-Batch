6eb512f7290c4beffeeccecdc4af40f36b758e2e
Shresth Rajput <rajshresth2@gmail.com>
2024-06-14 11:45:34 +0530

    Lecture on Introduction to Array

---
+++ Day-15 Array Inbuild Methods/script.js
@@ -0,0 +1,36 @@
+//1. push()
+// let arr=[1,2,3,4]
+// console.log(arr.push(5,6))
+// const retVal=arr.push(5,6)
+// console.log(arr)
+// console.log(retVal)
+
+// 2. pop()
+// let arr=[1,2,3,4,5,6]
+// const res=arr.pop()
+// console.log(arr)
+// console.log(res)
+
+
+// 3. unshift()
+// let actors=["Salmon","SRK","Akshay"]
+// const res=actors.unshift("Sidharth","Varun")
+// console.log(actors)
+// console.log(res)
+
+// 4. shift()
+// let places=['Russia','Manali','Kashmir','Kerala']
+// const res=places.shift()
+// console.log(places)
+// console.log(res)
+
+// 5. indexOf
+// let yt=['Carry','TRS','Elvish','TVS','BB']
+// let res=yt.indexOf('Carry',1)
+// console.log(res)
+
+// 6 includes()
+// let yt=['carry','TRS','Elvish','TVS','BB']
+
+// const res=yt.includes('carry',1)
+// console.log(res)
---
+++ Day-15 Array in Js/script.js
@@ -0,0 +1,26 @@
+// let arr=new Array(10,20,30,40,50)
+// console.log(arr)
+
+
+// let arr=[10,20,30]
+// console.log(arr.length)
+
+// last_index=arr.length-1
+// console.log(arr[arr.length-1])
+
+
+
+// array_object_reference[index]
+
+
+// let arr=[10,20,30,40]
+// console.log(arr)
+// arr.flat()
+// arr.push()
+// arr.at()
+// arr.concat()
+
+// function flat(){
+
+//     return 'Aba jaba jaba'
+// }
