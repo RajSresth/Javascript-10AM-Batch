70e3ed3da0a1dadceb5d016ed404bd23ddd26a4a
Shresth Rajput <rajshresth2@gmail.com>
2024-06-19 19:16:07 +0530

    Lecture on forEach Method

---
+++ Day-16 Array Inbuild Methods Part-2/script.js
@@ -0,0 +1,71 @@
+// 10. sort((a,b)=>a-b)
+
+// let arr=[9,7,5,10,3,8,6,2,4,1]
+// arr.sort((a,b)=>a-b)
+// console.log(arr)
+
+// For Ascending (a,b)=>a-b
+// For Ascending (a,b)=>b-a
+
+// 11. forEach(callback)
+// const temp=[]
+// const arr=[1,2,3,4,5]
+
+// arr.forEach((value,index,array)=>{
+//     let result=value*10
+//     temp.push(result)
+// })
+// console.log(arr)
+// console.log(temp)
+
+
+// const arr=[10,20,30,40,50]
+// let sum=0;
+
+// arr.forEach(val=>{
+//     sum=sum+val;
+// })
+// console.log(arr)
+// console.log(`Sum is: ${sum}`)
+
+
+// const arr=[100,200,300,400,500]
+// const output=[10,20,30,40,50]
+
+
+// const arr=[2,4,6,8,10]
+// const output=[4,16,36,64,100]
+
+
+// const arr=[1,2,3,4,5]
+// const output=[1,2,6,24,120]
+// const temp=[]
+
+// function findFactorial(a){
+//         let fact=1
+//         for(let i=1;i<=a;i++){
+//             fact=fact*i
+//         }
+//     return fact
+// }
+
+// arr.forEach(val=>{
+//     let result=findFactorial(val)
+//     temp.push(result)
+// })
+// console.log(temp)
+
+
+// let arr=[1,2,3,4,5]
+// arr.forEach((value,index,array)=>{
+//     array[index]=value*10
+// })
+// console.log(arr)
+
+
+const array=[1,2,3,4,5,6,7,8,9,10]
+const output=[2,4,6,8,10,18,21,24,27,30]
+
+
+
+
