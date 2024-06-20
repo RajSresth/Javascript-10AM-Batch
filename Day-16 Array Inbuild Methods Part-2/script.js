b87f069f93bf4dcfaf387be03b2e5f3e06b806f4
Shresth Rajput <rajshresth2@gmail.com>
2024-06-20 11:38:00 +0530

    Lecture on Map Method

--- Day-16 Array Inbuild Methods Part-2/script.js
+++ Day-16 Array Inbuild Methods Part-2/script.js
@@ -63,9 +63,58 @@
 // console.log(arr)
 
 
-const array=[1,2,3,4,5,6,7,8,9,10]
-const output=[2,4,6,8,10,18,21,24,27,30]
+
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+
+// const generic=(array,mid,n1,n2)=>{
+    //   array.splice(0,mid).
+    //   forEach((val,index)=>{
+        //     array.splice(index,0,val*n1)
+        //     })
+        
+        //     array.splice(mid,arr.length-mid)
+        //     .forEach((val,index)=>{
+            //         array[mid+index]=val*n2
+            //     })
+            
+            //     return array
+            // }
+            // const a=+prompt('First Number')
+            // const b=+prompt('Second Number')
+            // const half=Math.floor(arr.length/2)
+            // const output=generic(arr,half,a,b)
+            // console.log(output)
+            
+
+// const arr=[1,2,3,4,5,6,7,8,9,10]
+
+// const mid=Math.floor(arr.length/2) //5
+
+// arr.forEach((val,index,array)=>{
+//    if(index < mid){
+//         array[index]=val*2
+//    }
+//    else{
+//     array[index]=val*3
+//    }
+// })
+// console.log(arr)
 
 
+let arr=[1,2,3,4,5]
+output=[1,4,9,16,25]
+
+const result=arr.map((val,index,array)=>val*10)
+console.log(arr)
+console.log(result)
+    
+let arr=[1,2,3,4,5]
+output=[11,12,13,14,15]
 
 
+let marks=[100,90,80,70,60]
+let output=[50,45,40,35,30]
+
+// write a js program to create a function which accepts an array and flat them into single array?
+let array=[1,[2,3],[4,[5,6],7],[8,9],10]
+let output=[1,2,3,4,5,6,7,8,9,10]
