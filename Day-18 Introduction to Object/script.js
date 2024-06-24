be2a471d42f4938b38c24b96ac284938f2a6a9c2
Shresth Rajput <rajshresth2@gmail.com>
2024-06-24 11:40:28 +0530

    Lecture on Introduction to Object

---
+++ Day-18 Introduction to Object/script.js
@@ -0,0 +1,67 @@
+// const obj={
+//     "full name":"Lokesh Rajput",
+//     age:22,
+//     city:"Noida",
+//     state:'UP',
+//     isWorking:false
+// }
+// console.log(obj.age)
+// console.log(obj.isWorking)
+// console.log(obj["full name"])
+
+
+// const key1=prompt('Enter first key')
+// const key2=prompt('Enter second key')
+// const key3=prompt('Enter third key')
+
+// const user={
+//  [key1]:'Ravi',
+//  [key2]:23,
+//  [key3]:'noida'
+// }
+// console.log(user)
+
+
+// const mob=8700966758;
+// const state='Delhi'
+
+// const user=
+// {
+//     userId:12,
+//     userName:'Chombu',
+//     mob,
+//     state    
+// }
+// const user1=
+// {
+//     mob,
+//     state
+// }
+// console.log(user)
+
+// const userId=+prompt('Enter user id')
+// const userName=prompt('Enter your Name');
+// const age=+prompt('Enter your age');
+// const isWorking=Boolean(prompt('are you working: true| false'));
+
+
+// const user=
+// {
+//     userId,
+//     userName,
+//     age,
+//     isWorking
+// }
+// console.log(user)
+
+
+// Topic: ways to create an object
+
+const obj={
+    name:'Chombu',
+    age:206,
+    city:'Noida'
+}
+
+
+console.log(obj)
